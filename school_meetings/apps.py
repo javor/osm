e93c95c5c79c210344ab861e44364518e4f8279c
Janusz Jaworski <javor1992@gmail.com>
2018-03-03 15:29:28 +0100

    Initial commit - django project

---
+++ school_meetings/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchoolMeetingsConfig(AppConfig):
+    name = 'school_meetings'
---
+++ school_profiles/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchoolProfilesConfig(AppConfig):
+    name = 'school_profiles'
