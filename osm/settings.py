6ddbf2d099d61ce800db0a33a330dab31c3f4063
Janusz Jaworski <javor1992@gmail.com>
2018-03-04 23:03:13 +0100

    Add semantic-ui to static files.

--- osm/settings.py
+++ osm/settings.py
@@ -114,5 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/static/'
 
 AUTH_USER_MODEL = 'school_profiles.User'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
