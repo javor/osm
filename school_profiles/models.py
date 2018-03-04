7912a39e490e9e81d3f72dbc13f9c2ca6a56ee78
Janusz Jaworski <javor1992@gmail.com>
2018-03-04 22:55:35 +0100

    Change place of birth from TextField to CharField.

--- school_profiles/models.py
+++ school_profiles/models.py
@@ -30,7 +30,7 @@ class StudentProfile(UserProfile):
     # address = TODO: add address model (in this case it is required field)
     date_of_birth = models.DateField(blank=False, null=False, verbose_name=_('date of birth'))
     pesel = models.CharField(blank=False, max_length=9, null=False, verbose_name=_('PESEL'))
-    place_of_birth = models.TextField(verbose_name=_('place of birth'))
+    place_of_birth = models.CharField(max_length=255, verbose_name=_('place of birth'))
     parents_profiles = models.ManyToManyField(ParentProfile, blank=True, related_name='children',
                                               verbose_name=_('parent profile'))
     user = models.OneToOneField(User, blank=True, db_column='user_id', on_delete=models.PROTECT, null=True,
